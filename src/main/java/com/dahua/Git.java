3768b9ad6828589387e4024a6fd4a4defa137707
zzz <z18435798571@163.com>
2021-04-15 19:09:29 +0800

    第一次测试 oo

---
+++ src/main/java/com/dahua/Git.java
@@ -0,0 +1,11 @@
+package com.dahua;
+
+public class Git {
+
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+    }
+
+}
---
+++ src/main/java/com/dahua/Git01.java
@@ -0,0 +1,11 @@
+package com.dahua;
+
+public class Git01 {
+
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 50;
+
+    }
+
+}
